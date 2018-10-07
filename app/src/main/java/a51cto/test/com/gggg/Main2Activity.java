8e2929ecc5b82f30f4b8e7875dcb6b0fb81d6a9f
Qiaoshi96 <2642656099@qq.com>
2018-10-07 22:36:12 +0800

    增加底部导航栏

---
+++ app/src/main/java/a51cto/test/com/gggg/Main2Activity.java
@@ -0,0 +1,44 @@
+package a51cto.test.com.gggg;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+        //尝试谷歌自带的可以创建底部导航栏的button
+    private TextView mTextMessage;
+    //自带底部点击事件
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
--- app/src/main/java/a51cto/test/com/gggg/MainActivity.java
+++ app/src/main/java/a51cto/test/com/gggg/MainActivity.java
@@ -2,9 +2,12 @@ package a51cto.test.com.gggg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//重新创建github仓库
+//其实很多时候超级难过因为不知道自己想要的是什么也不知道自己这个样子可以坚持多久反正就是不知道
+//得有自己的追求知道自己想要的是什么然后一直坚持下去
+//一定要相信自己啊乔石
+//你可以的对不对不要堕落
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
